06265899da56e1cf4bd4e5532c9d587f3ef29fd5
Mark Lennox <mark@webpusher.ie>
2018-11-15 09:05:15 +0000

    adding formik and building a very simple form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import "./App.css"
+
+const DisplayErrorMessage = message => <span className="error">{message}</span>
 
 class App extends Component {
+  handleValidation = values => {
+    let errors = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
+  }
+  handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={this.handleValidation}
+        onSubmit={this.handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <label>
+              Email
+              <Field type="email" name="email" placeholder="Please provide your email" />
+              <ErrorMessage name="email" render={DisplayErrorMessage} />
+            </label>
+            <label>
+              Password
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" render={DisplayErrorMessage} />
+            </label>
+            <label>
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </label>
+          </Form>)}
+      </Formik>
     );
   }
 }
