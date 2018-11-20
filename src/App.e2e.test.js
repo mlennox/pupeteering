941d4947ab8ba5c48d1cf23eb0fb27e51f7f68cd
Mark Lennox <mark@webpusher.ie>
2018-11-20 23:31:20 +0000

    creating page every test

--- src/App.e2e.test.js
+++ src/App.e2e.test.js
@@ -1,24 +1,16 @@
 import puppeteer from 'puppeteer';
 
-let page, browser = null;
+let browser = null;
 
 beforeAll(async () => {
   try {
     // /home/circleci/repo/node_modules/puppeteer/.local-chromium
-    // browser = await puppeteer.launch({ headless: true });
-    browser = await puppeteer.launch({ headless: true, executablePath: '/home/circleci/repo/node_modules/puppeteer/.local-chromium' });
+    browser = await puppeteer.launch({ headless: true });
+    // browser = await puppeteer.launch({ headless: true, executablePath: '/home/circleci/repo/node_modules/puppeteer/.local-chromium' });
   }
   catch (e) {
     console.log('= = = = = = = BEFORE ALL ERROR - BROWSER', e);
     if (browser) await browser.close();
-  }
-
-  try {
-    page = await browser.newPage();
-  }
-  catch (e) {
-    console.log('= = = = = = = BEFORE ALL ERROR - PAGE', e);
-    if (page) await page.close();
   }
 });
 
@@ -31,15 +23,27 @@ describe('App tests', () => {
     const passwordInput = '[data-testid="password"]';
     const password_errormessage = '[data-testid="password_label"] .error';
 
-    beforeAll(async () => {
-      await page.goto('http://localhost:3000/')
-    });
+    // beforeAll(async () => {
+    //   await page.goto('http://localhost:3000/')
+    // });
 
     afterAll(async () => {
       browser.close();
     });
 
+    // beforeEach(() => {
+    //   try {
+    //     page = await browser.newPage();
+    //   }
+    //   catch (e) {
+    //     console.log('= = = = = = = BEFORE ALL ERROR - PAGE', e);
+    //     if (page) await page.close();
+    //   }
+    // })
+
     test('not adding an email address causes a "required" error message', async () => {
+      const page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
       await page.click(emailInput);
       await page.click(passwordInput); // no blur, so click elsewhere instead
 
@@ -49,6 +53,8 @@ describe('App tests', () => {
     });
 
     test('not providing a passsword causes a "required" error message', async () => {
+      const page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
       await page.click(passwordInput);
       await page.click(emailInput); // no blur, so click elsewhere instead
 
@@ -58,6 +64,8 @@ describe('App tests', () => {
     });
 
     test('bad email address generates an error message', async () => {
+      const page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
       await page.click(emailInput);
       await page.type(emailInput, "bademailaddress");
       await page.click(passwordInput); // no blur, so click elsewhere instead
@@ -68,6 +76,8 @@ describe('App tests', () => {
     });
 
     test('proper email addresss will pass validation', async () => {
+      const page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
       await page.click(emailInput);
       await page.type(emailInput, "good@email.com");
       await page.click(passwordInput); // no blur, so click elsewhere instead
