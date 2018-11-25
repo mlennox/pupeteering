54451b52240ddaf597bfc47b801523103a7f7089
Mark Lennox <mark@webpusher.ie>
2018-11-25 18:03:40 +0000

    more curl checks

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
-# yarn start &
-# sleep 10
-# curl http://localhost:3000
+yarn start &
+sleep 10
+curl http://localhost:3000
 # https://discuss.circleci.com/t/memory-problems-with-jest-and-workers/10297
 yarn test --maxWorkers 2
 # sleep 10
